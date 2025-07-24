7eef1ebae813f9f5e68ebf65bc2cf990a3cce7b4
ribondareva <msbondareva01@gmail.com>
2025-07-25 00:18:02 +0300

    feat(routes): add login, verify, and profile views to web urls

--- users/urls_web.py
+++ users/urls_web.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views_web import LoginView, VerifyView, ProfileView
 
 urlpatterns = [
-    path('', LoginView.as_view(), name="login"),
+    path('login/', LoginView.as_view(), name="login"),
     path('verify/', VerifyView.as_view(), name="verify"),
     path('profile/', ProfileView.as_view(), name="profile"),
 ]
