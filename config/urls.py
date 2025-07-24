8ef1108e322ae2e5a3d56f5785fa425d703bd394
ribondareva <msbondareva01@gmail.com>
2025-07-25 00:17:50 +0300

    feat(routes): add Swagger and ReDoc routes to main urls

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,13 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.shortcuts import redirect
+from django.conf.urls.static import static
+from config import settings
+
+
+def redirect_login(request):
+    return redirect("login")
 
 
 schema_view = get_schema_view(
@@ -27,3 +34,7 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+urlpatterns += [
+    path('accounts/login/', redirect_login),
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
